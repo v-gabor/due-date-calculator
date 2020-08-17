57b1b4a255391d46cefe21386bde9a16f8bc2f72
v-gabor <varadi.gabor@optimadesign.hu>
2020-08-17 22:35:26 +0200

    Check weekends, #4

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
+import { InvalidReportCreationTimeError } from "./errors";
+import { runInThisContext } from "vm";
+
 class DueDateCalculator {
-  calculateDueDate({
-    createdAt,
-    turnaroundTime,
-  }: IssueData): ResolvedIssueData {
+  issueCreatedAt: Date;
+  turnaroundTime: number;
+
+  constructor({ createdAt, turnaroundTime }: IssueData) {
+    this.issueCreatedAt = new Date(createdAt);
+    this.turnaroundTime = turnaroundTime;
+  }
+
+  calculateDueDate(): ResolvedIssueData {
+    this.checkWeekdays();
+
     return {
-      resolvedAt: "now",
+      resolvedAt: "now!",
     };
+  }
+
+  /**
+   * This method check the day. Saturday and sunday are invalid days.
+   */
+  checkWeekdays() {
+    if (
+      this.issueCreatedAt.getDay() == 0 ||
+      this.issueCreatedAt.getDay() == 6
+    ) {
+      throw new InvalidReportCreationTimeError(
+        "Saturday and sunday are not valid days!"
+      );
+    }
   }
 }
 
 export const CalculateDueDate = (issueData: IssueData): ResolvedIssueData => {
-  const calculator = new DueDateCalculator();
+  const calculator = new DueDateCalculator(issueData);
 
-  return calculator.calculateDueDate(issueData);
+  return calculator.calculateDueDate();
 };
